c61ae67360a28a07de0be16d6737b0ef1ed17b8b
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-03 11:36:49 -0300

    Starting studing arrays

--- java-data-structors/src/dataStructors/javaArrays.java
+++ java-data-structors/src/dataStructors/javaArrays.java
@@ -3,8 +3,23 @@ package dataStructors;
 public class javaArrays {
 
 	public static void main(String[] args) {
-		System.out.println("Arrays");
-
+		String[] cars = {"Camaro","Corvette", "Tesla","BMW"};
+		System.out.println(cars[0]);
+		cars[0] = "Mustang";
+		System.out.println(cars[0]);
+		System.out.println(cars[3]);
+		System.out.println();
+		System.out.println();
+		// or
+		
+		String[] otherCars = new String[3];
+		otherCars[0] = "Tesla";
+		otherCars[1] = "BWM";
+		otherCars[2] = "Fusca";
+		
+		for(int i=0; i<otherCars.length;i++) {
+			System.out.println(otherCars[i]);
+		}
 	}
 
 }
