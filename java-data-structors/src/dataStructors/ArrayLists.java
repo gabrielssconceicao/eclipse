1ef21f0fdb44bf38e3c6e332288497bdc06b460f
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-03 12:49:07 -0300

    Starting ArrayLists

---
+++ java-data-structors/src/dataStructors/ArrayLists.java
@@ -0,0 +1,11 @@
+package dataStructors;
+
+public class ArrayLists {
+
+	public static void main(String[] args) {
+		// ArrayLists - a resizeble array
+		// 				Elements can be added and removed after compilation phase
+		//				store reference data types
+	}
+
+}
