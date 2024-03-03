36af4b8561c8cea4c2f68b81597bf9616adaff75
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-03 12:55:50 -0300

    View some of ArrayList methods

--- java-data-structors/src/dataStructors/ArrayLists.java
+++ java-data-structors/src/dataStructors/ArrayLists.java
@@ -1,11 +1,23 @@
 package dataStructors;
-
+import java.util.ArrayList;
 public class ArrayLists {
 
 	public static void main(String[] args) {
 		// ArrayLists - a resizeble array
 		// 				Elements can be added and removed after compilation phase
 		//				store reference data types
+		
+		ArrayList<String> food = new ArrayList<String>();
+		food.add("pizza");
+		food.add("hamburger");
+		food.add("hotdog");
+		food.set(0, "sushi");
+		food.remove(2);
+		// food.clear() - clear the ArrayList
+		for(int i=0; i<food.size();i++) {
+			System.out.println(food.get(i));
+		}
 	}
+	
 
 }
