aa73e190b223d3dd1fffd4b23a38020e0dfbdecf
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-09 12:23:28 -0300

    entrada de dados

--- Sequential-Structure/src/App.java
+++ Sequential-Structure/src/App.java
@@ -1,7 +1,12 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+    }
+
+    static private void saidaDeDados() {
         double x = 32.32556;
         int y = 32;
         String name = "John";
@@ -17,5 +22,32 @@ public class App {
         System.out.printf("The value of x is %.4f\n", x);
 
         System.out.printf("%s tem %d anos, e ganha R$%.2f reais %n", name, age, renda);
+    }
+
+    static private void entradaDeDados() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        char w;
+        String x;
+        int y;
+        double z;
+
+        System.out.println("Digite algo: ");
+        x = sc.nextLine();
+        System.out.println("Você escreveu: " + x + "\n");
+
+        System.out.println("Digite um caractere: ");
+        w = sc.next().charAt(0);
+        System.out.println("O caractere digitado é: " + w + "\n");
+
+        System.out.println("Digite um valor: ");
+        y = sc.nextInt();
+        System.out.println("O valor digitado é: " + y + "\n");
+
+        System.out.println("Digite um valor decimal: ");
+        z = sc.nextDouble();
+        System.out.println("O valor digitado é: " + z + "\n");
+        sc.close();
     }
 }
