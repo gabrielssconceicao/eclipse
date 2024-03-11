1705a69a4878aad2dda6130dc3002a4e1791cf1f
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-11 14:52:48 -0300

    Interpolation Search

--- Data-Structures-and-Algorithms/src/algorithms/InterpolationSearch.java
+++ Data-Structures-and-Algorithms/src/algorithms/InterpolationSearch.java
@@ -1,10 +1,45 @@
 package algorithms;
 
-public class InterpolationSearch {
+public class InterpolationSearch extends CreateArray {
   // interpolation search = Search algorithm that finds the position of a target
   // value within a sorted array.
+  // interpolation search = improvement over binary search best used for
+  // "uniformly" distributed data
+  // "guesses" where a value might be based on calculated probe results
+  // if probe is incorrect, search area is narrowed, and a new probe is calculated
 
+  // average case: O(log(log(n)))
+  // worst case: O(n) [values increase exponentially]
   public static void main(String[] args) {
+    int[] array = createArray(10);
 
+    int index = interpolationSearch(array, 1);
+
+    if (index != -1) {
+      System.out.println("Element found at index: " + index);
+    } else {
+      System.out.println("Element not found");
+    }
+  }
+
+  private static int interpolationSearch(int[] array, int value) {
+    int heigh = array.length - 1;
+    int low = 0;
+
+    while (value >= array[low] && value <= array[heigh] && low <= heigh) {
+      int probe = low + (heigh - low) * (value - array[low]) / (array[heigh] - array[low]);
+
+      System.out.println("Probe: " + probe);
+
+      if (array[probe] == value) {
+        return probe;
+      } else if (array[probe] < value) {
+        low = probe + 1;
+      } else {
+        heigh = probe - 1;
+      }
+    }
+
+    return -1;
   }
 }
