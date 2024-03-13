33e8667b7e028d2bec0f45391266234c2e2c483f
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-13 16:04:41 -0300

    methods, contructors and overload

--- OOP/src/Conta.java
+++ OOP/src/Conta.java
@@ -1,7 +1,39 @@
+import java.util.Objects;
+
 public class Conta {
 
   Person titular;
   int agencia;
   int numero;
   double saldo;
+
+  Conta(Person titular, int agencia, int numero) {
+    Objects.requireNonNull(titular);
+    this.titular = titular;
+    this.agencia = agencia;
+    this.numero = numero;
+  }
+
+  void depositar(double value) {
+    if (value < 0) {
+      throw new IllegalArgumentException("Depositos devem ser positivos!");
+    }
+    this.saldo += value;
+  }
+
+  void sacar(double value) {
+    if (value < 0) {
+      throw new IllegalArgumentException("Saque devem ser positivos!");
+    }
+    if (saldo - value < 0) {
+      throw new IllegalStateException("Saldo insuficiente!");
+    }
+
+    saldo -= value;
+  }
+
+  void sacar(double value, double taxaSaque) {
+    sacar(value + taxaSaque);
+
+  }
 }
