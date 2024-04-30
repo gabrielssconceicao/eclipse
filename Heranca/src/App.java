9937e1c83d9af05dcbf54ea5fc7c2b87efcc4c32
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-04-30 17:14:02 -0300

    herança

--- Heranca/src/App.java
+++ Heranca/src/App.java
@@ -1,5 +1,8 @@
+import entities.BusinessAccount;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BusinessAccount businessAccount = new BusinessAccount();
+
     }
 }
