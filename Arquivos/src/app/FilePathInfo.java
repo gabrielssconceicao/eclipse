86293b3704a1c34fb8262a8d758d29ad59fed3b7
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-05-12 13:22:24 -0300

    Getting file path information

---
+++ Arquivos/src/app/FilePathInfo.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FilePathInfo {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter file path: ");
+    String strPath = sc.nextLine();
+
+    File path = new File(strPath);
+
+    System.out.println("Get name: " + path.getName());
+    System.out.println("Get parent: " + path.getParent());
+
+    System.out.println("Get absolute path: " + path.getAbsolutePath());
+    sc.close();
+  }
+}
