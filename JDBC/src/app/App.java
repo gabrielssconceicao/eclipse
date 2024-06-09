92145993fc732e745c71b0962cddc56e7f3f27ce
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-09 16:33:25 -0300

    Seller: reusing instantiation

--- JDBC/src/app/App.java
+++ JDBC/src/app/App.java
@@ -8,6 +8,8 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
     }
