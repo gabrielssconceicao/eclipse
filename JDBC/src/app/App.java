c055d9db9428a0e22649850c4dabfd5a5b007450
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-09 16:48:11 -0300

    Seller: fingByDepartment implementation

--- JDBC/src/app/App.java
+++ JDBC/src/app/App.java
@@ -1,7 +1,10 @@
 package app;
 
+import java.util.List;
+
 import models.dao.DAOFactory;
 import models.dao.SellerDAO;
+import models.entities.Department;
 import models.entities.Seller;
 
 public class App {
@@ -9,8 +12,15 @@ public class App {
 
         SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 
-        System.out.println("=== TEST 1: seller findById ===");
+        System.out.println("\n=== TEST 1: seller findById ===\n");
         Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===\n");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDAO.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
--- JDBC/src/models/dao/SellerDAO.java
+++ JDBC/src/models/dao/SellerDAO.java
@@ -2,6 +2,7 @@ package models.dao;
 
 import java.util.List;
 
+import models.entities.Department;
 import models.entities.Seller;
 
 public interface SellerDAO {
@@ -15,4 +16,6 @@ public interface SellerDAO {
   Seller findById(Integer id);
 
   List<Seller> findAll();
+
+  List<Seller> findByDepartment(Department department);
 }
