bdfb006fdded80ec4edf632f83f99dd6349de30e
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-09 15:29:12 -0300

    Seller entity class

--- JDBC/src/app/App.java
+++ JDBC/src/app/App.java
@@ -1,10 +1,14 @@
 package app;
 
+import java.util.Date;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 1000.0, obj);
+        System.out.println(seller);
     }
 }
